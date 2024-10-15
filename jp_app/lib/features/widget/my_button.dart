7957d419701ca1abd66ebec3b72bc6fb4382daf8
dennis <info.dennis.durmus@gmail.com>
2024-10-15 09:44:42 +0200

    Button in Widgets Ordner Ausgelagert

---
+++ jp_app/lib/features/widget/my_button.dart
@@ -0,0 +1,42 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:jp_app/home_screen.dart';
+import 'package:jp_app/features/mainscreen/main_screen.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.pink,
+              const Color.fromARGB(255, 255, 156, 64).withOpacity(0.8),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        child: const Text(
+          "Order Now",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
